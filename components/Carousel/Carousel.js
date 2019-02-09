3d860afa33dea55d00b647689a0f015fe8f6b1ee
millan-figueroa <millan.fig@gmail.com>
2019-02-09 11:56:27 -0800

    re-did carousel.js, added slideNext and slideBack functions

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,89 +1,53 @@
 class Carousel {
-    constructor(urls) {
-        // Create HTML:
-        // create div with class "carousel"
-        // create div with class "left-button" and append that to div above.
-    
-        /* 
-            <div class="carousel">
-                <div class="left-button"><</div>
-                <img src="./assets/carousel/mountains.jpeg" />
-                <img src="./assets/carousel/computer.jpeg" />
-                <img src="./assets/carousel/trees.jpeg" />
-                <img src="./assets/carousel/turntable.jpeg" />
-                <div class="right-button">></div>
-            </div>
-        */
-    
-        const carouselDiv = document.createElement("div");
-        carouselDiv.classList.add("carousel");
-    
-        const leftButtonDiv = document.createElement("div");
-        leftButtonDiv.classList.add("left-button");
-        leftButtonDiv.textContent = "<";
-        carouselDiv.appendChild(leftButtonDiv);
-    
-        urls.forEach(url => {
-          const image = document.createElement("img");
-          carouselDiv.appendChild(image);
-          console.log(url);
-          image.src = url;
-        });
-    
-        const rightButtonDiv = document.createElement("div");
-        rightButtonDiv.classList.add("right-button");
-        rightButtonDiv.textContent = ">";
-        carouselDiv.appendChild(rightButtonDiv);
-    
-        this.element = carouselDiv;
-        console.log("carousel");
-    
-        // Create a current index
-        this.currentImageIndex = 0;
-    
-        this.images = this.element.querySelectorAll(".carousel img");
-        console.log(this.images);
-    
-        // display first image as default
-        this.images[this.currentImageIndex].style.display = "block";
-    
-        // const rightButton = this.element.querySelector(".right-button");
-        // console.log(rightButton);
-        rightButtonDiv.addEventListener("click", () => this.slideNext());
-    
-        // const leftButton = this.element.querySelector(".left-button");
-        // console.log(leftButton);
-        leftButtonDiv.addEventListener("click", () => this.slideBack());
-      }
-    
-      slideNext() {
-        console.log("slide next clicked");
-        this.images[this.currentImageIndex].style.display = "none";
-        this.currentImageIndex++;
-    
-        // at the end of all images, reset to 0
-        if (this.currentImageIndex === this.images.length) {
-          this.currentImageIndex = 0;
-        }
-    
-        this.images[this.currentImageIndex].style.display = "block";
-      }
-    
-      slideBack() {
-        console.log("slide back clicked");
-        this.images[this.currentImageIndex].style.display = "none";
-        this.currentImageIndex--;
-    
-        // if at the first image go to the last image
-        if (this.currentImageIndex === -1) {
-          this.currentImageIndex = this.images.length - 1;
-        }
-        this.images[this.currentImageIndex].style.display = "block";
-      }
+    constructor(element) {
+      this.element = element;
+      console.log("carousel");
+  
+      // Create a current index
+      this.currentImageIndex = 0;
+  
+      this.images = this.element.querySelectorAll(".carousel img");
+      console.log(this.images);
+      // display first image as default
+      this.images[this.currentImageIndex].style.display = "block";
+  
+      const rightButton = this.element.querySelector(".right-button");
+      console.log(rightButton);
+      rightButton.addEventListener("click", () => this.slideNext());
+  
+      const leftButton = this.element.querySelector(".left-button");
+      console.log(leftButton);
+      leftButton.addEventListener("click", () => this.slideBack());
     }
-
-
-let carousel = document.querySelector();
+    slideNext() {
+      console.log("slide next clicked");
+      this.images[this.currentImageIndex].style.display = "none";
+      this.currentImageIndex++;
+  
+      // at the end of all images, reset to 0
+      if (this.currentImageIndex === this.images.length) {
+        this.currentImageIndex = 0;
+      }
+  
+      this.images[this.currentImageIndex].style.display = "block";
+    }
+  
+    slideBack() {
+      console.log("slide back clicked");
+      this.images[this.currentImageIndex].style.display = "none";
+      this.currentImageIndex--;
+  
+      // if at the first image go to the last image
+      if (this.currentImageIndex === -1) {
+        this.currentImageIndex = this.images.length - 1;
+      }
+      this.images[this.currentImageIndex].style.display = "block";
+    }
+  }
+  
+  let carousels = document.querySelectorAll(".carousel");
+  carousels.forEach(carousel => new Carousel(carousel));
+  
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
