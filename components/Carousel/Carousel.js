75991186436f33db23a6abe42814e9373fab558f
millan-figueroa <millan.fig@gmail.com>
2019-02-15 19:50:25 -0800

    Re-did index.html, removed extra cards and fixed some syntax errors

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,59 +1,55 @@
 class Carousel {
-    constructor(element) {
-      this.element = element;
-      console.log("carousel");
-  
-      // Create a current index
-      this.currentImageIndex = 0;
-  
-      this.images = this.element.querySelectorAll(".carousel img");
-      console.log(this.images);
-      // display first image as default
-      this.images[this.currentImageIndex].style.display = "block";
-  
-      const rightButton = this.element.querySelector(".right-button");
-      console.log(rightButton);
-      rightButton.addEventListener("click", () => this.slideNext());
-  
-      const leftButton = this.element.querySelector(".left-button");
-      console.log(leftButton);
-      leftButton.addEventListener("click", () => this.slideBack());
-    }
-    slideNext() {
-      console.log("slide next clicked");
-      this.images[this.currentImageIndex].style.display = "none";
-      this.currentImageIndex++;
-  
-      // at the end of all images, reset to 0
-      if (this.currentImageIndex === this.images.length) {
-        this.currentImageIndex = 0;
-      }
-  
-      this.images[this.currentImageIndex].style.display = "block";
-    }
-  
-    slideBack() {
-      console.log("slide back clicked");
-      this.images[this.currentImageIndex].style.display = "none";
-      this.currentImageIndex--;
-  
-      // if at the first image go to the last image
-      if (this.currentImageIndex === -1) {
-        this.currentImageIndex = this.images.length - 1;
-      }
-      this.images[this.currentImageIndex].style.display = "block";
-    }
+  constructor(carousel){
+
+      this.carousel = carousel;
+      this.leftButton = this.carousel.querySelector('.left-button');
+      this.rightButton = this.carousel.querySelector('.right-button');
+      this.images = this.carousel.querySelectorAll('img')
+
+      let carouselIndex = 0;
+      this.carouselIndex = carouselIndex;
+      this.images[carouselIndex].style.display = 'flex';
+      // this.images.forEach(image => image.style.display = 'flex');
+
+      this.leftButton.addEventListener('click', () => this.selectLeft(carouselIndex));
+      this.rightButton.addEventListener('click', () => this.selectRight(carouselIndex));
   }
-  
-  let carousels = document.querySelectorAll(".carousel");
-  carousels.forEach(carousel => new Carousel(carousel));
-  
+
+  // selectLeft(index) {
+  //     this.images[this.carouselIndex].style.display = 'none';
+  //     if (this.carouselIndex <= 0) {this.carouselIndex = this.images.length}
+  //     this.carouselIndex--;
+  //     this.images[this.carouselIndex].style.display = 'flex';
+  // }
+  // selectRight(index) {
+  //     this.images[this.carouselIndex].style.display = 'none';
+  //     if (this.carouselIndex >= this.images.length-1) {this.carouselIndex = -1}
+  //     this.carouselIndex++;
+  //     this.images[this.carouselIndex].style.display = 'flex';
+  // }
+
+  selectLeft(index) {
+      $(this.images[this.carouselIndex]).hide();
+      if (this.carouselIndex <= 0) {this.carouselIndex = this.images.length}
+      this.carouselIndex--;
+      $(this.images[this.carouselIndex]).show('slow');
+  }
+  selectRight(index) {
+      $(this.images[this.carouselIndex]).hide();
+      if (this.carouselIndex >= this.images.length-1) {this.carouselIndex = -1}
+      this.carouselIndex++;
+      $(this.images[this.carouselIndex]).show('slow');
+  }
+}
+
+let carousel = document.querySelector('.carousel')
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
+  1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+  2. You will need to grab a reference to all of the images
+  3. Create a current index
+  4. Those buttons are gonna need some click handlers.
+  5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+  6. Have fun!
 */
